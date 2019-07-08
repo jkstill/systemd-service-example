8c8e2fee4ea22e2acc9bced7a1227ab7421bff32
Jared Still <jkstill@gmail.com>
2019-07-08 15:16:24 -0700

    simplified the service

--- reverse-service.pl
+++ reverse-service.pl
@@ -15,32 +15,6 @@ use Socket qw(SOL_SOCKET SO_RCVBUF IPPROTO_IP IP_TTL);
 
 my $port = 4242;
 
-# now setting bufsz dynamically via message from the client
-# so this bit is not really necessary - leaving it here for now though
-
-my $bufsz=$ARGV[0];
-
-$bufsz = 2048 unless $bufsz;
-
-# minimal sanity check for bufsz
-
-unless ( $bufsz =~ /^[[:digit:]]+$/ ) {
-	die "bufsz of $bufsz is not an integer\n";
-}
-
-# bufsz really should be a power of 2
-# $log will be an integer if a power of 2
-my $log = log($bufsz) / log(2);
-unless ( $log =~ /^[[:digit:]]+$/ ) {
-
-	die "bufsz of $bufsz is not a power of 2\n";
-}
-
-# bufsz should be LT 8M
-if ($bufsz > (8 * 2**20) ) {
-	die "bufszs of $bufsz is GT 8M (8388608)\n";
-}
-
 $| = 1; # flush stdout
  
 my $proto = getprotobyname('tcp');    #get the tcp protocol
@@ -52,14 +26,11 @@ my $sock = IO::Socket::INET->new(
 	Reuse => 1
 ) or die "Cannot create socket: $@";
 
-$sock->setsockopt(SOL_SOCKET, SO_RCVBUF, $bufsz) or
-	die "setsockopt: $!";
 
 print "Initial Receive Buffer is ", $sock->getsockopt(SOL_SOCKET, SO_RCVBUF),
 	" bytes\n";
 
-print "Server is now listening ...\n";
-print "Initial Buffer size set to: $bufsz\n";
+print "Server reverse-text is now listening ...\n";
  
 $SIG{INT} = sub { shutdown $sock,2; close($sock); die "\nkilled\n" };
 
@@ -79,14 +50,9 @@ while(1)
 		print "Could not lookup name for port $clientPort\n";
 	}
 
- 
-	#binmode $client, ':bytes';
-	#binmode $sock, ':bytes';
-	
-	my $line;
-	while(my $r=sysread($client,$line,$bufsz)) {
-		syswrite($client, scalar reverse $line);
-		syswrite($client, "\n");
+	while(<$client>) {
+		my $line = $_;
+		syswrite($client, scalar reverse $line );
 	}
 
 	shutdown $client,2;
